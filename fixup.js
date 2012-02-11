c1b7be62996eaae11e55ca851fb98bc92872e68a
Jeremy Phillippe <0xutxvf02@sneakemail.com>
2012-02-10 18:18:15 -0700

    Whoops, leftover function.

--- fixup.js
+++ fixup.js
@@ -9,24 +9,6 @@ String.prototype.replaceAll = function (target, replacement) {
     return (tx);
 }
 
-var ratingButtons = function () {
-		$('.threadrate img').each(function (idx) {
-			var src = $(this).attr('src');
-			if (src.match(/\/rate1/i)) {
-				$(this).addClass('r1');
-			} else if (src.match(/\/rate2/i)) {
-				$(this).addClass('r2');
-			} else if (src.match(/\/rate3/i)) {
-				$(this).addClass('r3');
-			} else if (src.match(/\/rate4/i)) {
-				$(this).addClass('r4');
-			} else if (src.match(/\/rate5/i)) {
-				$(this).addClass('r5');
-			} else if (src.match(/\/digg/i)) {
-				$(this).addClass('digg');
-			}});
-};
-
 /* Supporting code */
 $(document).ready(function (x) {
   /* Fix for weird layout-breaking forums javascript (thanks Radium!) */
